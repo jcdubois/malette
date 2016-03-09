3af52ae22e0bd42e5d4db4d86dc26d85a230789b
Jean-Christophe Dubois <jcd@tribudubois.net>
2016-03-09 20:06:04 +0100

    Add a delay after each data or address setting.
    This should allow the board to latch the pins.
    
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- malette/Bus.cpp
+++ malette/Bus.cpp
@@ -2,6 +2,9 @@
 #include "Bus.h"
 #include <Arduino.h>
 
+#define DEBUG 1
+#define BUS_DELAY 2
+
 static int addressPins[8] = {22, 24, 26, 28, 30, 32, 34, 36};
 static int dataPins[8] = {23, 25, 27, 29, 31, 33, 35, 37};
 
@@ -13,13 +16,16 @@ bool Bus::Initialize() {
   // Initialize all pins as output
   for (int i = 0; i < 8; i++) {
     pinMode(addressPins[i], OUTPUT);
+    digitalWrite(addressPins[i], LOW);
     pinMode(dataPins[i], OUTPUT);
+    digitalWrite(dataPins[i], LOW);
   }
 }
 
 void Bus::Write(unsigned char decoder, unsigned char channel,
                 unsigned char data) {
 
+#if DEBUG
   Serial.print("WriteData: ");
   for (int i = 7; i >= 0; i--) {
     Serial.print(data & (1 << i) ? '1' : '0');
@@ -29,13 +35,15 @@ void Bus::Write(unsigned char decoder, unsigned char channel,
   Serial.print(" , ");
   Serial.print(channel);
   Serial.println(')');
+#endif
 
-  // write the data to the data bus
+  // write the data value to the data bus
   WriteData(data);
 
   // write the address to the address bus
   WriteAddress(decoder, channel);
 
+  // Write 0 to the data bus
   WriteData(0);
 }
 
@@ -46,17 +54,21 @@ void Bus::WriteAddress(unsigned char decoder, unsigned char channel) {
       digitalWrite(addressPins[i + 5], HIGH);
     }
   }
+  delay(BUS_DELAY);
 
   // Select our decoder
   digitalWrite(addressPins[decoder], HIGH);
+  delay(BUS_DELAY);
 
   // Deselect the decoder
   digitalWrite(addressPins[decoder], LOW);
+  delay(BUS_DELAY);
 
   // Clear the 3 higher bits = channel
   for (int i = 0; i < 4; i++) {
     digitalWrite(addressPins[i + 5], LOW);
   }
+  delay(BUS_DELAY);
 }
 
 void Bus::WriteData(unsigned char data) {
@@ -64,4 +76,5 @@ void Bus::WriteData(unsigned char data) {
   for (int i = 0; i < 8; i++) {
     digitalWrite(dataPins[i], data & (1 << i) ? HIGH : LOW);
   }
+  delay(BUS_DELAY);
 }
