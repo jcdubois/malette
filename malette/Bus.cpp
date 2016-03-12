8e0d20a72597bfa6b389a74d6ad286f4920aa005
Jean-Christophe Dubois <jcd@tribudubois.net>
2016-03-12 15:55:20 +0100

    Rework Bus timeout
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- malette/Bus.cpp
+++ malette/Bus.cpp
@@ -21,10 +21,10 @@
 #include <Arduino.h>
 
 #define DEBUG 1
-#define BUS_DELAY 2
+#define BUS_DELAY() delay(2)
 
-static int addressPins[8] = {22, 24, 26, 28, 30, 32, 34, 36};
-static int dataPins[8] = {23, 25, 27, 29, 31, 33, 35, 37};
+static unsigned char addressPins[8] = {22, 24, 26, 28, 30, 32, 34, 36};
+static unsigned char dataPins[8] = {23, 25, 27, 29, 31, 33, 35, 37};
 
 Bus::Bus() {}
 
@@ -42,7 +42,6 @@ bool Bus::Initialize() {
 
 void Bus::Write(unsigned char decoder, unsigned char channel,
                 unsigned char data) {
-
 #if DEBUG
   Serial.print("WriteData: ");
   for (int i = 7; i >= 0; i--) {
@@ -72,21 +71,21 @@ void Bus::WriteAddress(unsigned char decoder, unsigned char channel) {
       digitalWrite(addressPins[i + 5], HIGH);
     }
   }
-  delay(BUS_DELAY);
+  BUS_DELAY();
 
   // Select our decoder
   digitalWrite(addressPins[decoder], HIGH);
-  delay(BUS_DELAY);
+  BUS_DELAY();
 
   // Deselect the decoder
   digitalWrite(addressPins[decoder], LOW);
-  delay(BUS_DELAY);
+  BUS_DELAY();
 
   // Clear the 3 higher bits = channel
   for (int i = 0; i < 3; i++) {
     digitalWrite(addressPins[i + 5], LOW);
   }
-  delay(BUS_DELAY);
+  BUS_DELAY();
 }
 
 void Bus::WriteData(unsigned char data) {
@@ -94,5 +93,5 @@ void Bus::WriteData(unsigned char data) {
   for (int i = 0; i < 8; i++) {
     digitalWrite(dataPins[i], data & (1 << i) ? HIGH : LOW);
   }
-  delay(BUS_DELAY);
+  BUS_DELAY();
 }
